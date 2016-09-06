f99ce3a467a4daff684e05e7dfa0cec7d3a177bf
Peter Molnar <peter@petermolnar.hu>
2016-09-06 22:07:53 +0200

    Change in the sha1 algorithm.

--- Fyber-TestApp/PMOHashGenerator.m
+++ Fyber-TestApp/PMOHashGenerator.m
@@ -11,21 +11,20 @@
 @implementation PMOHashGenerator
 
 + (NSString *)generateSHA1FromString:(NSString *)baseString {
-    const char *cStr = [baseString UTF8String];
-    unsigned char result[CC_SHA512_DIGEST_LENGTH];
-    CC_SHA1(cStr, strlen(cStr), result);
-    if (result) {
-        /* SHA-1 hash has been calculated and stored in 'result'. */
-        
-        NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH];
-        
-        for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
-            [output appendFormat:@"%02x", result[i]];
-        return output;
-        
+    NSData *data = [baseString dataUsingEncoding:NSUTF8StringEncoding];
+    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
+    
+    CC_SHA1(data.bytes, data.length, digest);
+    
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
+    
+    for (int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
+    {
+        [output appendFormat:@"%02x", digest[i]];
     }
-    return nil;
+    
+    return output;
+
     
 }
-
 @end
