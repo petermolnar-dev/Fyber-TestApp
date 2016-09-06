2f04da23dffe22fe6ec7ede74c45f718b97bc18e
Peter Molnar <peter@petermolnar.hu>
2016-09-06 21:46:11 +0200

    Nw algorithm to Hash generation

--- Fyber-TestApp/PMOHashGenerator.m
+++ Fyber-TestApp/PMOHashGenerator.m
@@ -11,19 +11,20 @@
 @implementation PMOHashGenerator
 
 + (NSString *)generateSHA1FromString:(NSString *)baseString {
-    const char *cstr = [baseString cStringUsingEncoding:NSUTF8StringEncoding];
-    NSData *data = [NSData dataWithBytes:cstr length:baseString.length];
-    
-    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
-    
-    CC_SHA1(data.bytes, data.length, digest);
-    
-    NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
-    
-    for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
-        [output appendFormat:@"%02x", digest[i]];
-    
-    return output;
+    const char *cStr = [baseString UTF8String];
+    unsigned char result[CC_SHA512_DIGEST_LENGTH];
+    CC_SHA1(cStr, strlen(cStr), result);
+    if (result) {
+        /* SHA-1 hash has been calculated and stored in 'result'. */
+        
+        NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH];
+        
+        for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
+            [output appendFormat:@"%02x", result[i]];
+        return output;
+        
+    }
+    return nil;
     
 }
 
